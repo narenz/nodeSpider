dfb37224a3b519d37d1ed6541c262f3a70d70467
Narendra Bellala <Narendra.Bellala@talktalkplc.com>
2017-04-06 13:20:56 +0100

    regex added

--- index.js
+++ index.js
@@ -2,15 +2,19 @@ const req = require('request');
 const URL_Parse = require('url-parse');
 const cheerio = require('cheerio');
 
-const URL_TO_CRAWL = "https://google.com";
+const URL_TO_CRAWL = "https://gocardless.com";
+const URL_REGEX = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)';
+var allAbsoluteLinks = [];
 
 req(URL_TO_CRAWL, (error, response, body) => {
-    if(!error && response.statusCode === 200) {
+    if (!error && response.statusCode === 200) {
         const $ = cheerio.load(body)
         // console.log("Page title:  " + $('title').text());
-
-        const pageLinks = $("a[href^='http']");
-
-        console.log(pageLinks);
+        var absoluteLinks = $("[href^='http']");
+        
+        absoluteLinks.each(function () {
+            allAbsoluteLinks.push($(this).attr('href'));
+        });
+        console.log(allAbsoluteLinks);
     }
 });
