07596b0a7389bce55a50615841bca8f91cde4301
Narendra Bellala <Narendra.Bellala@talktalkplc.com>
2017-04-07 12:32:56 +0100

    Added relative link

--- index.js
+++ index.js
@@ -1,20 +1,23 @@
+"use strict"
 const req = require('request');
 const URL_Parse = require('url-parse');
 const cheerio = require('cheerio');
 
 const URL_TO_CRAWL = "https://gocardless.com";
 const URL_REGEX = 'https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)';
-var allAbsoluteLinks = [];
+var allRelativeLinks = [];
 
 req(URL_TO_CRAWL, (error, response, body) => {
     if (!error && response.statusCode === 200) {
         const $ = cheerio.load(body)
         // console.log("Page title:  " + $('title').text());
-        var absoluteLinks = $("[href^='https:\/\/gocardless.com']");
-        
-        absoluteLinks.each(function () {
-            allAbsoluteLinks.push($(this).attr('href'));
+        let links =  $("a[href^='/']");
+        $(links).each(function (i, link) {
+            console.log($(link).text() + ':\t  ' + $(link).attr('href'));
+            allRelativeLinks = $(link).attr('href')
         });
-        console.log(allAbsoluteLinks);
+    }
+    else {
+        console.log(response.statusCode);
     }
 });
