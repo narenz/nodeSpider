1166598d67ba77079d7ae69b8ab39586f915d36e
Narendra Bellala <Narendra.Bellala@talktalkplc.com>
2017-04-10 08:16:06 +0100

    Added coverage and tidy up

--- src/extractAssets.js
+++ src/extractAssets.js
@@ -4,8 +4,10 @@ exports = module.exports = ($, ele, atr) => {
     let selectors = $(ele);
     let assets = [];
     $(selectors).each((i, link) => {
-        if ($(link).attr(atr))
+        if ($(link).attr(atr))  //TO avoid empty assets
+        {
             assets.push($(link).attr(atr));
+        }
     });
     return assets;
 }
--- test/test.js
+++ test/test.js
@@ -1,13 +1,32 @@
 import 'babel-polyfill';
 import * as chai from 'chai';
+import cheerio from 'cheerio';
 
 let assert = chai.assert;
 let should = chai.should();
 let expect = chai.expect;
-import * as extractor from '../src/extractAssets.js';
+let Extractor = require('../src/extractAssets.js');
 
 describe('extractor', function() {
-	it('should extract assets from a valid HTML', function() {
-         console.log(extractor);
+
+    it('shoule return empty array when no assets found', () =>{
+        const $ = cheerio.load("<body></body>");
+        let assets = new Extractor($, 'img', 'src');
+        assets.should.be.a('array');
+        assets.should.have.lengthOf(0);
+    })
+
+    it('shoule return empty array when no src attribute is empty', () =>{
+        const $ = cheerio.load("<body><img src=''></body>");
+        let assets = new Extractor($, 'img', 'src');
+        assets.should.be.a('array');
+        assets.should.have.lengthOf(0);
+    })
+
+    it('should extract assets from a valid HTML', function () {
+        const $ = cheerio.load("<body><img src='smiley.gif'></body>");
+        let assets = new Extractor($, 'img', 'src');
+        assets.should.be.a('array');
+        assets.should.have.lengthOf(1);
     });
 });
